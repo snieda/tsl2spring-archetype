387bb75a29a2aadeceeefbd5653ca3021c10d41f
Thomas Schneider <snieda@web.de>
2019-09-09 23:01:51 +0200

    correction in classPrefix variable

--- create-mda-app.sh
+++ create-mda-app.sh
@@ -1,4 +1,4 @@
-mvn archetype:generate -B -DarchetypeGroupId=tsl2.spring -DarchetypeArtifactId=mda-archetype -DgroupId=$1 -DartifactId=$2 -DapplicationPort=8080 -DdatabasePort=9092
+mvn archetype:generate -B -DarchetypeGroupId=net.sf.tsl2nano -DarchetypeArtifactId=mda-spring-archetype -DgroupId=$1 -DartifactId=$2 -DapplicationPort=8080 -DdatabasePort=9092
 
 cd $2
 mvn spring-boot:run
