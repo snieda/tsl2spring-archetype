ef5d7f20e29975e485593aef505e16d3e9b273a8
Thomas Schneider <snieda@web.de>
2019-10-03 20:26:43 +0200

    +css +login

--- create-mda-app.sh
+++ create-mda-app.sh
@@ -1,4 +1,5 @@
-mvn archetype:generate -B -DarchetypeGroupId=net.sf.tsl2nano -DarchetypeArtifactId=mda-spring-archetype -DgroupId=$1 -DartifactId=$2 -DapplicationPort=8080 -DdatabasePort=9092
+#ARCHETYPE_VERSION= -DarchetypeVersion=0.0.2-SNAPSHOT
+mvn archetype:generate -B -DarchetypeGroupId=net.sf.tsl2nano -DarchetypeArtifactId=mda-spring-archetype $ARCHETYPE_VERSION -DgroupId=$1 -DartifactId=$2 -DapplicationPort=8080 -DdatabasePort=9092 $3 $4 $5 $6
 
 cd $2
-mvn spring-boot:run
+mvn install spring-boot:run
