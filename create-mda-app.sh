49b354e97c87ca7d1242396a57ad3318e5e3997f
Thomas Schneider <snieda@web.de>
2019-11-03 11:35:21 +0100

    +idType, parent: tsl2nano 2.3.1-SNAPSHOT

--- create-mda-app.sh
+++ create-mda-app.sh
@@ -1,5 +1,6 @@
-#ARCHETYPE_VERSION= -DarchetypeVersion=0.0.2-SNAPSHOT
-mvn archetype:generate -B -DarchetypeGroupId=net.sf.tsl2nano -DarchetypeArtifactId=mda-spring-archetype $ARCHETYPE_VERSION -DgroupId=$1 -DartifactId=$2 -DapplicationPort=8080 -DdatabasePort=9092 $3 $4 $5 $6
+#ARCHETYPE_VERSION=-DarchetypeVersion=0.0.2-SNAPSHOT
+ARCHETYPE_LOCAL=-DarchtypeCatalog=local
+mvn archetype:generate -B -DarchetypeGroupId=net.sf.tsl2nano -DarchetypeArtifactId=mda-spring-archetype $ARCHETYPE_VERSION $ARCHETYPE_LOCAL -DgroupId=$1 -DartifactId=$2 -DapplicationPort=8080 -DdatabasePort=9092 -DidType=java.lang.Long $3 $4 $5 $6
 
 cd $2
 mvn install spring-boot:run
